5e82687cffb25e7dff97ee62fa667726c4b9ec7d
lidor ettinger <lidor.ettinger@gmail.com>
2022-04-28 00:02:42 +0300

    fix returning func

--- src/org/foo/functions/k8s.groovy
+++ src/org/foo/functions/k8s.groovy
@@ -6,13 +6,13 @@ def helmDeploy(Map args) {
                     ${args.chart} \
                     -n ${args.namespace} \
                     --version ${args.version} \
-                    --wait""", returnStdout: true) ? true : throw e
+                    --wait""", returnStdout: true) ? true : false
 }
 
 def helmConfig(Map args) {
     if (!args.chart) args.chart = "bitnami"
     if (!args.chart_url) args.chart_url = "https://charts.bitnami.com/bitnami"
-    return sh(script: "helm repo add ${args.chart} ${args.chart_url}", returnStdout: true) ? true : throw e
+    return sh(script: "helm repo add ${args.chart} ${args.chart_url}", returnStdout: true) ? true : false
 }
 
 return this
