2f8594f7e43a37c482fbcfabb74d866427764687
lidor ettinger <lidor.ettinger@gmail.com>
2022-05-25 00:54:34 +0300

    adding jq curl

--- src/org/foo/functions/github.groovy
+++ src/org/foo/functions/github.groovy
@@ -3,6 +3,7 @@ package org.foo.functions;
 def getGitAuthor(Map args=[:]) {
     if (!args.containerName) args.containerName = "git"
     container(args.containerName) {
+        sh (script: 'git config --global --add safe.directory ${WORKSPACE}', returnStdout: true).trim()
         return sh (script: 'git log -1 --pretty=%cn ${GIT_COMMIT}', returnStdout: true).trim()
     }
 }
